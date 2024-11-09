2e34a1f13badbb9a857c8c9bacbceb332b4dbc03
Ekaterina Danilenko <keitilins@yandex.ru>
2024-11-09 15:36:22 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
+  // if (process.env.NODE_ENV !== "development") {
+  //   return;
+  // }
 
   const { worker } = await import("~shared/mocks/browser");
 
